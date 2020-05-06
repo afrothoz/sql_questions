b57e82405dbb898f0e65ee130d6d5f85492dcbce
afrothoz <51341540+afrothoz@users.noreply.github.com>
2020-05-06 22:54:07 +0100

    Questions 1 -3

---
+++ answers.sql
@@ -0,0 +1,17 @@
+SELECT student_name
+FROM student
+WHERE student_name = 'John';
+
+SELECT COUNT(score.student_id)
+FROM score
+INNER JOIN subject
+ON score.subject_id = subject.subject_id
+WHERE score.score > 75;
+
+SELECT student_id, student_name
+FROM student as st
+INNER JOIN score as sc
+ON st.student_id = sc.student_id
+INNER JOIN on subject as su
+ON sc.subject_id = su.subject_id
+WHERE su.subject_name = 'English';
