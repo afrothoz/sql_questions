65d5d32b8ef00ec9c3cc51360fdecd11c845758f
afrothoz <51341540+afrothoz@users.noreply.github.com>
2020-05-06 22:55:42 +0100

    comment the questions

--- answers.sql
+++ answers.sql
@@ -1,13 +1,16 @@
+--Question 1
 SELECT student_name
 FROM student
 WHERE student_name = 'John';
 
+--Question 2
 SELECT COUNT(score.student_id)
 FROM score
 INNER JOIN subject
 ON score.subject_id = subject.subject_id
 WHERE score.score > 75;
 
+--Question 3
 SELECT student_id, student_name
 FROM student as st
 INNER JOIN score as sc
